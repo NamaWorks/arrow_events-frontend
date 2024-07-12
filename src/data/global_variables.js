fc87c7f48d0aad7fbda97bf6d820afe773740fcc
NamaWorks <pedronayamartinez@hotmail.com>
2024-07-12 20:30:06 +0200

    hotfix api direction

--- src/data/global_variables.js
+++ src/data/global_variables.js
@@ -1,4 +1,5 @@
 // DOM ELEMENTS
 export const app = document.querySelector("#app")
-export const api = "http://localhost:3500/"
+// export const api = "http://localhost:3000/"
+export const api = "http://localhost:3000/"
 
