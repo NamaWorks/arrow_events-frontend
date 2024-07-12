a36c8b000450db30da3a07a37b707e9e066fa080
NamaWorks <pedronayamartinez@hotmail.com>
2024-07-12 20:35:06 +0200

    hotifx api direction  2

--- src/data/global_variables.js
+++ src/data/global_variables.js
@@ -1,5 +1,5 @@
 // DOM ELEMENTS
 export const app = document.querySelector("#app")
 // export const api = "http://localhost:3000/"
-export const api = "http://localhost:3000/"
+export const api = "https://arrow-events-backend.vercel.app/"
 
